9c37a6ece9f871cd89d2d80b782e7ffceb21690c
PT Trijaya Digital Grup <terusberkreasi@gmail.com>
2025-05-19 15:35:40 +0700

    update responsive

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,33 @@
     class="h-screen w-screen flex items-center justify-center bg-gradient-to-br from-white to-blue-100 overflow-hidden relative"
   >
     <div
-      class="flex z-20 flex-col md:flex-row items-center w-full max-w-7xl relative overflow-hidden"
+      class="flex z-20 flex-col md:flex-row items-center w-full max-w-6xl relative overflow-hidden px-10"
     >
       <div class="flex-1">
-        <div class="flex items-center mb-14">
-          <img src="@/assets/logo_long.png" alt="wrapper" class="w-[16rem]" />
+        <div class="flex justify-center md:justify-start items-center mb-14">
+          <img src="@/assets/logo_long.png" alt="wrapper" class="w-[22rem]" />
         </div>
         <h1
-          class="text-5xl md:text-6xl font-bold text-gray-900 mb-16 leading-tight"
+          class="text-5xl md:text-7xl text-center md:text-left font-bold text-gray-900 mb-8 md:mb-16 leading-tight"
         >
           Coming Soon
         </h1>
-        <p class="text-lg text-slate-600 max-w-xl">
+        <p
+          class="text-lg text-slate-600 max-w-xl text-center md:text-left mb-10"
+        >
           Ada yang diam-diam bergerak di Ngawi, tunggu saja...
         </p>
       </div>
       <div
-        class="flex-1 flex justify-center mt-12 md:mt-0 relative size-[30rem]"
+        class="flex-1 flex justify-center mt-12 md:mt-0 relative height-[25rem]"
       >
         <div class="absolute inset-0 flex items-center justify-center pb-10">
-          <img src="@/assets/logo.png" alt="wrapper" class="w-[16rem]" />
+          <img src="@/assets/logo.png" alt="wrapper" class="w-[14rem]" />
         </div>
         <img
           src="@/assets/logo_wrapper.png"
           alt="wrapper"
-          class="size-[30rem]"
+          class="size-[25rem]"
         />
       </div>
     </div>
