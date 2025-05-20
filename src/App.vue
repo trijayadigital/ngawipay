1044c1800c84232212a8e379ed21d7bb270994ff
Umar Sani <63645229+umarsani1605@users.noreply.github.com>
2025-05-20 19:40:26 +0700

    add music

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue';
+
+const audio = new Audio('/kartonyono_medot_janji.mp3');
+audio.loop = true; // Agar lagu diputar berulang
+
+onMounted(() => {
+  // Menambahkan event listener untuk memainkan audio setelah interaksi user pertama
+  const playAudio = () => {
+    audio.play();
+    document.removeEventListener('click', playAudio);
+  };
+  document.addEventListener('click', playAudio);
+});
+</script>
 
 <template>
-  <section
-    class="h-screen w-screen flex items-center justify-center bg-gradient-to-br from-white to-blue-100 overflow-hidden relative"
-  >
-    <div
-      class="flex z-20 flex-col md:flex-row items-center w-full max-w-6xl relative overflow-hidden px-10"
-    >
+  <section class="h-screen w-screen flex items-center justify-center bg-gradient-to-br from-white to-blue-100 overflow-hidden relative">
+    <div class="flex z-20 flex-col md:flex-row items-center w-full max-w-6xl relative overflow-hidden px-10">
       <div class="flex-1">
         <div class="flex justify-center md:justify-start items-center mb-14">
           <img src="@/assets/logo_long.png" alt="wrapper" class="w-[22rem]" />
         </div>
         <h1
-          class="text-5xl md:text-7xl text-center md:text-left font-bold bg-gradient-to-r from-[#0096FF] to-[#5BCFE8] text-transparent bg-clip-text mb-8 md:mb-16 leading-tight h-[6rem]"
+          class="text-6xl md:text-7xl text-center md:text-left font-bold bg-gradient-to-r from-[#0096FF] to-[#49c1ee] text-transparent bg-clip-text mb-2 sm:mb-4 md:mb-12 leading-tight md:h-[6rem] sm:h-[7rem] h-[9rem] xs:h-[10rem]"
         >
           Coming Soon
         </h1>
-        <p
-          class="text-lg text-slate-600 max-w-xl text-center md:text-left mb-10"
-        >
-          Ada yang diam-diam bergerak di Ngawi, tunggu saja...
-        </p>
+        <p class="text-lg text-slate-600 max-w-xl text-center md:text-left mb-10">Ada yang diam-diam bergerak di Ngawi, tunggu saja...</p>
       </div>
-      <div
-        class="flex-1 flex justify-center mt-12 md:mt-0 relative size-[20rem] md:size-[25rem]"
-      >
+      <div class="flex-1 flex justify-center mt-12 md:mt-0 relative size-[20rem] md:size-[25rem]">
         <div class="absolute inset-0 flex items-center justify-center pb-10">
-          <img
-            src="@/assets/logo.png"
-            alt="wrapper"
-            class="w-[12rem] md:w-[14rem]"
-          />
+          <img src="@/assets/logo.png" alt="wrapper" class="w-[12rem] md:w-[14rem]" />
         </div>
-        <img
-          src="@/assets/logo_wrapper.png"
-          alt="wrapper"
-          class="size-[20rem] md:size-[25rem]"
-        />
+        <img src="@/assets/logo_wrapper.png" alt="wrapper" class="size-[20rem] md:size-[25rem]" />
       </div>
     </div>
-    <div
-      class="absolute z-10 -left-80 size-[64rem] rounded-full bg-blue-100/80 blur-3xl"
-    ></div>
+    <div class="absolute z-10 -left-80 size-[64rem] rounded-full bg-blue-100/80 blur-3xl"></div>
   </section>
 </template>
 
